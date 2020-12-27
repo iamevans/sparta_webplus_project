9d68b9760e48031f9e35ad589e28204b732edbdc
iamevans <evans@sungil-i.kr>
2020-12-27 11:10:58 +0900

    003 commit

--- project3/app.py
+++ project3/app.py
@@ -12,6 +12,11 @@ db = client.dbsparta_plus_week3
 def main():
     return render_template("index.html")
 
+@app.route('/map')
+def test_map():
+    return render_template("prac_map.html")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
+
