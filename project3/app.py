21b9ec64428305b42565fa25f4714574d62c04d0
iamevans <evans@sungil-i.kr>
2020-12-28 13:29:41 +0900

    004 commit

--- project3/app.py
+++ project3/app.py
@@ -12,9 +12,13 @@ db = client.dbsparta_plus_week3
 def main():
     return render_template("index.html")
 
-@app.route('/map')
-def test_map():
-    return render_template("prac_map.html")
+@app.route('/matjip', methods=["GET"])
+def get_matjip():
+    # 맛집 목록을 반환하는 API
+    matjip_list = list(db.matjips.find({}, {'_id': False}))
+    # matjip_list 라는 키 값에 맛집 목록을 담아 클라이언트에게 반환합니다.
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
